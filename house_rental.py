07484049d119b9e76ddf3495a305debaeb79b41e
daijun_chen <daijun.chen@nuance.com>
2018-09-23 12:28:11 -0400

    add house and purchase_rental modules

--- house_rental.py
+++ house_rental.py
@@ -1,4 +1,7 @@
 # Create a class named HouseRental, a subclass of House and Rental #
+from house import House
+from purchase_rental import Rental
+
 class HouseRental(House, Rental):
     def prompt_init():
         init = House.prompt_init()
